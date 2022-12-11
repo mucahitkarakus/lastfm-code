7f30dd29c3cf41ef68e9afadcdf67e9ba465062a
mucahitkarakus <mocococo.80@gmail.com>
2022-12-11 17:47:48 +0300

    detailcard return button

---
+++ src/components/Button/Return.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {BsArrowReturnLeft} from "react-icons/bs";
+
+const Return = () => {
+  return (
+    <div>
+      <BsArrowReturnLeft />
+    </div>
+  );
+};
+
+export default Return;
--- src/pages/DetailCard.jsx
+++ src/pages/DetailCard.jsx
@@ -1,34 +1,42 @@
 import React from "react";
+import {BsArrowReturnLeft} from "react-icons/bs";
+
 import TopAlbumList from "../components/DetailList/TopAlbumList";
 import TopTracks from "../components/DetailList/TopTracks";
 
 const DetailCard = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-[35rem] ">
-      <div className="flex flex-col items-center bg-gradient-to-r from-pink-600 w-[60vw] p-4 h-[40rem] text-white">
-        <div className="flex ml-4 border p-5 h-[7rem] w-full mt-8 ">
-          <img
-            src="https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
-            alt=""
-            className="w-[5rem] h-[5rem] "
-          />
-          <h2 className="ml-5 flex justify-center items-center">
-            Travis Scott
-          </h2>
-        </div>
-        <div className="flex justify-between w-full mt-8 ml-4 ">
-          <div className="w-[45%]">
-            <h2>TopAlbums</h2>
-            <hr />
-            <div className="mt-11 w-full h-[20rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-pink-300">
-              <TopAlbumList />
-            </div>
+    <div>
+      <div>
+        <BsArrowReturnLeft />
+      </div>
+
+      <div className="flex items-center justify-center w-screen h-[35rem] ">
+        <div className="flex flex-col items-center bg-gradient-to-r from-pink-600 w-[60vw] p-4 h-[40rem] text-white">
+          <div className="flex ml-4 border p-5 h-[7rem] w-full mt-8 ">
+            <img
+              src="https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png"
+              alt=""
+              className="w-[5rem] h-[5rem] "
+            />
+            <h2 className="ml-5 flex justify-center items-center">
+              Travis Scott
+            </h2>
           </div>
-          <div className="w-[45%]">
-            <h2>Top Tracks</h2>
-            <hr />
-            <div className="mt-11 w-full h-[20rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-pink-300">
-              <TopTracks />
+          <div className="flex justify-between w-full mt-8 ml-4 ">
+            <div className="w-[45%]">
+              <h2>TopAlbums</h2>
+              <hr />
+              <div className="mt-11 w-full h-[20rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-pink-300">
+                <TopAlbumList />
+              </div>
+            </div>
+            <div className="w-[45%]">
+              <h2>Top Tracks</h2>
+              <hr />
+              <div className="mt-11 w-full h-[20rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-pink-300">
+                <TopTracks />
+              </div>
             </div>
           </div>
         </div>
