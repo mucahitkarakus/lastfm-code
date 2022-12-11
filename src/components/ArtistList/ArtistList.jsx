ce05a9afa5da6a55707c89ee66f00d4a877f8b39
mucahitkarakus <mocococo.80@gmail.com>
2022-12-11 17:39:27 +0300

    a

--- src/components/ArtistList/ArtistList.jsx
+++ src/components/ArtistList/ArtistList.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const ArtistList = () => {
   return (
     <div>
-      {" "}
       <div className="flex items-center justify-between  bg-gradient-to-l from-pink-700 w-full h-[6rem] mt-3 cursor-pointer">
         <div className="flex ml-2">
           <img src="" className="w-[5rem] h-[5rem]" alt="" />
