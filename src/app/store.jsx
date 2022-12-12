e075e20c6b25e56dee7fce39a29d1c1c702f0e09
mucahitkarakus <mocococo.80@gmail.com>
2022-12-12 03:32:23 +0300

    update store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import artistReducer from "../features/artistSlice";
+import albumReducer from "../features/albumSlice";
+import trackReducer from "../features/trackSlice";
 
 const store = configureStore({
   reducer: {
     artists: artistReducer,
+    albums: albumReducer,
+    tracks: trackReducer,
   },
 });
 
