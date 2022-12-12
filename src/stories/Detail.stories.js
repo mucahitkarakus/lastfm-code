e20f94c85198b68c5b225e06770ae760d34e8d85
mucahitkarakus <mocococo.80@gmail.com>
2022-12-12 10:02:13 +0300

    storybook albumitem

--- src/stories/Detail.stories.js
+++ src/stories/Detail.stories.js
@@ -1,3 +1,59 @@
 import React from 'react'
-import TopAlbumList from '../components/DetailList/TopAlbumList';
+import ArtistList from '../components/ArtistList/ArtistList';
 
+const AlbumItem = {
+    image: [{
+        '#text':
+        'https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png',
+        size: "medium"
+     },
+    {
+        '#text':
+        'https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png',
+        size: "medium"
+     },
+    {
+        '#text':
+        'https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png',
+        size: "medium"
+     },
+    {
+        '#text':
+        'https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png',
+        size: "medium"
+     },
+    {
+        '#text':
+        'https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png',
+        size: "medium"
+     },    
+    ],
+    name:"Undefined",
+    playcoun: 135,
+    url:'https://www.last.fm/music/Undefined'
+};
+
+// export default{
+//     title: "album",
+//     component: ArtistList,
+// };
+
+// const Template = (args) => <ArtistList {...args} />;
+
+// export const Drake = Template.bind({});
+// Drake.args={
+//     item: AlbumItem,
+//     name: "Drake"
+// };
+
+// export const TaylorSwift= Template.bind({});
+// TaylorSwift.args={
+//     item:AlbumItem,
+//     name: "Taylor Swift"  
+// }
+
+// export const TheWeeknd = Template.bind({});
+// TheWeeknd.args={
+//     item:AlbumItem,
+//     name:"The Weeknd"
+// }
